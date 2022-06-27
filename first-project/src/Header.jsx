13624cb5e34de7573cd5344b00f163e91263a1dd
amatveevprog <amatveev.prog@gmail.com>
2022-06-27 04:29:05 +0300

    initial 2

---
+++ first-project/src/Header.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Header(){
+    return(
+        <header>
+            <h1>This is Reddit for our own hot reload is &</h1>
+        </header>
+    )
+}
