499b2148413771ac4c3b869abb954fe4aa656d0d
amatveevprog <amatveev.prog@gmail.com>
2022-07-07 23:00:15 +0300

    webpack5 initial

---
+++ first-project/src/Header.jsx
@@ -0,0 +1,15 @@
+// import { hot } from 'react-hot-loader/root';
+import * as React from "react";
+//const styles = require('./header.css');
+// import styles from './header.css';
+
+export function Header(){
+    // console.log(styles, styles.example);
+    return(
+        <header>
+            <h1>Reddit for our own and everything is new absolutely2!!!</h1>
+        </header>
+    )
+}
+
+// export const Header = hot(HeaderComponent);
---
+++ first-project/src/index.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Header } from './Header.jsx';
+
+
+// this is for SSR!
+window.addEventListener('load', () => {
+    ReactDom.render(<Header />, document.getElementById('react_root'));
+});
