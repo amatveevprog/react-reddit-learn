7bf4e6d601c3fa4d3c310c1a1ca0cdaa61732148
amatveevprog <amatveev.prog@gmail.com>
2022-07-07 23:02:21 +0300

    init

--- first-project/webpack.config.js
+++ first-project/webpack.config.js
@@ -1,7 +1,43 @@
-const clientConfig = require('./cfg/webpack.client.config');
-const serverConfig = require('./cfg/webpack.server.config');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = [
-  clientConfig,
-  serverConfig
-]
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'production';
+
+function setupDevtool() {
+  if (IS_DEV) return 'eval';
+  if (IS_PROD) return false;
+}
+
+module.exports = {
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    resolve: {
+        extensions: [
+          '.js', '.jsx', '.ts', '.tsx', '.json'
+        ],
+    },
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    module: {
+        rules: [{
+          test: /\.[tj]sx?$/,
+          exclude: /node_modules/,
+          use: ['ts-loader']
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html')
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        open: true, 
+        hot: IS_DEV
+    },
+    devtool: setupDevtool(),
+}
