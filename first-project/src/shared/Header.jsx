9d607e2e38c912d8dbb5ffc59c71576e1c51f8e3
amatveevprog <amatveev.prog@gmail.com>
2022-07-08 18:46:52 +0300

    hmr-2-3

--- first-project/src/shared/Header.jsx
+++ first-project/src/shared/Header.jsx
@@ -1,15 +1,15 @@
-// import { hot } from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader/root';
 import * as React from "react";
 //const styles = require('./header.css');
 // import styles from './header.css';
 
-export function Header(){
+function HeaderComponent(){
     // console.log(styles, styles.example);
     return(
         <header>
-            <h1>Reddit for our own and everything is new absolutely 123!!!</h1>
+            <h1>Reddit for our own and ev3erything i3s new absolutely 123!!!</h1>
         </header>
     )
 }
 
-// export const Header = hot(HeaderComponent);
+export const Header = hot(HeaderComponent);
